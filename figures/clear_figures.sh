de76bb76afadcdf248e9f0b00806fe82435e6213
Zachary L. Andalman <za9132@stellar-vis1.princeton.edu>
2023-10-27 13:19:57 -0400

    removed clear_figures

--- figures/clear_figures.sh
+++
@@ -1,6 +0,0 @@
-mv all/* legacy/all/
-mv alpha_eps0p01/* legacy/alpha_eps0p01/
-mv alpha_eps0p1/* legacy/alpha_eps0p1/
-mv alpha_eps1p0/* legacy/alpha_eps1p0/
-mv gas/* legacy/gas/
-mv dmo/* legacy/dmo/
